85477e4254d1fedf8b06794f417f05f3c578d780
庄俊霖 <zhuangjunlin@hupu.com>
2022-01-15 15:30:16 +0800

    fix: ts+jest, describe等api识别不出类型

--- tsx-rollup-jest/__test__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import getText from '../src/index'
-
-describe('getText', () => {
-  it('getText() === "hello world"', () => {
-    const text = getText()
-    expect(text).toBe('hello world')
-  })
-})
---
+++ tsx-rollup-jest/src/__test__/index.test.ts
@@ -0,0 +1,8 @@
+import getText from "../index";
+
+describe("getText", () => {
+  it('getText() === "hello world"', () => {
+    const text = getText();
+    expect(text).toBe("hello world");
+  });
+});
